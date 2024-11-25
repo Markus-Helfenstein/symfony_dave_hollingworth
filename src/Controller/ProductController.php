3b4e353af99e59cfc1b992c0af7b81c7c7a35d65
Markus Helfenstein <markus.helfenstein@gmail.com>
2024-11-25 15:58:44 +0100

    Added form with validation to create a new product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,6 +18,38 @@ class ProductController extends AbstractController
     {
         return $this->render('product/index.html.twig', [
             'products' => $productRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/product/{id<\d+>}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+
+    #[Route('/product/new', name: 'product_new')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($product);
+            $manager->flush();
+
+            $this->addFlash('notice', 'Product created successfully!');
+
+            return $this->redirectToRoute('product_show', [
+                'id' => $product->getId(),
+            ]);
+        }
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form
         ]);
     }
 }
